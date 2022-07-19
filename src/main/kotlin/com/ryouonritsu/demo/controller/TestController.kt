0b5c9d6ea4cd2097a74d91b64751ca07d0e2178e
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-07-19 23:52:07 +0800

    User Entity

---
+++ src/main/kotlin/com/ryouonritsu/demo/controller/TestController.kt
@@ -0,0 +1,10 @@
+package com.ryouonritsu.demo.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+    @GetMapping("/test")
+    fun test() = "test"
+}
---
+++ src/main/kotlin/com/ryouonritsu/demo/entity/User.kt
@@ -0,0 +1,18 @@
+package com.ryouonritsu.demo.entity
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+class User(
+    var username: String,
+    var password: String
+) {
+    constructor() : this("", "")
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val userId = 0L
+}
